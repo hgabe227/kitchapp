23e59d47c1fed72370024c7fce2cc140e7065d44
hgabe227 <hgabe227@gmail.com>
2026-08-29 16:59:04 -0500

    Create initialMeals array with meal data
    Added initial meal data with details for each meal.

---
+++ data/meals.ts
@@ -0,0 +1,47 @@
+import { Meal } from "@/types/kitchen";
+
+export const initialMeals: Meal[] = [
+  {
+    id: "m1",
+    name: "Chicken & Rice Bowls",
+    ingredients: [
+      "Chicken Breast",
+      "Rice",
+      "Black Beans",
+      "Tomatoes",
+    ],
+    servings: 10,
+  },
+  {
+    id: "m2",
+    name: "Chicken Pasta",
+    ingredients: [
+      "Chicken Breast",
+      "Pasta",
+      "Tomatoes",
+      "Cheese",
+    ],
+    servings: 10,
+  },
+  {
+    id: "m3",
+    name: "Bean & Rice Bowls",
+    ingredients: [
+      "Rice",
+      "Black Beans",
+      "Tomatoes",
+      "Cheese",
+    ],
+    servings: 10,
+  },
+  {
+    id: "m4",
+    name: "Tomato Cheese Pasta",
+    ingredients: [
+      "Pasta",
+      "Tomatoes",
+      "Cheese",
+    ],
+    servings: 10,
+  },
+];
